47d051eb0730552482da9795536649a46c531a20
Tozy Fullbuster <tozyfullbuster@gmail.com>
2022-11-11 21:47:31 +0700

    Add file-group task results

---
+++ file-group/results/2022-11-11_21-39-05/000012/BAI2/TEST01/BAI2.py
@@ -0,0 +1,24 @@
+import math
+
+
+def is_prime(num):
+  if num < 2:
+    return False
+  if num < 4:
+    return True
+  if num % 2 == 0:
+    return False
+  for i in range(5, int(math.sqrt(num)) + 1, 2):
+    if num % i == 0:
+      return False
+  return True
+
+
+with open("BAI2.IN", "r") as inp:
+  n = int(inp.read())
+
+with open("BAI2.OUT", "w") as out:
+  if is_prime(n):
+    out.write("true")
+  else:
+    out.write("false")
---
+++ file-group/results/2022-11-11_21-43-43/000013/BAI1/TEST03/BAI1.py
@@ -0,0 +1,18 @@
+def gdc(x, y):
+  if y == 0:
+    return x
+  return gdc(y, x % y)
+
+
+def lcm(x, y):
+  return int((x * y) / gdc(x, y))
+
+
+with open("BAI1.IN", "r") as inp:
+  parts = inp.read().split()
+
+a = int(parts[0])
+b = int(parts[1])
+
+with open("BAI1.OUT", "w") as out:
+  out.write("{} {}".format(gdc(a, b), lcm(a, b)))
