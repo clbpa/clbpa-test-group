05dd9725c35317c042ddb06195f6a3ac7003b702
Tozy Fullbuster <tozyfullbuster@gmail.com>
2022-11-13 14:28:10 +0700

    Fix user 000002 BAI1.cpp code

--- console-group/users/000002/BAI1.cpp
+++ console-group/users/000002/BAI1.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-#include <fstream>
-#include <algorithm>
 
 using namespace std;
+
+int gcdF(int x, int y) {
+  if (y == 0) return x;
+  
+  return gcdF(y, x % y);
+}
 
 int main() {
   int a, b;
   cin >> a >> b;
   
-  int gdc = __gcd(a, b);
-  int lcm = (a * b) / gdc;
+  int gcd = gcdF(a, b);
+  int lcm = (a * b) / gcd;
   
-  cout << gdc << " " << lcm;
+  cout << gcd << " " << lcm;
   return 0;
 }
+
